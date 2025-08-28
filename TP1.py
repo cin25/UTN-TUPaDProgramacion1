bb575dc0317e0e6f97ad4e06685334981f733b5f
cin25 <cin12590@gmail.com>
2025-08-28 07:16:30 -0300

    Add files via upload

---
+++ TP1.py
@@ -0,0 +1,43 @@
+#Ejercicio1
+print ("Hola mundo")
+
+#Ejercicio2
+nombre= input("¿Cual es tu nombre?")
+print(f"Hola {nombre} mucho gusto!")
+
+#Ejercicio3
+print("Ingrese los siguientes datos")
+
+edad= input("¿Cuantos años tiene?")
+nombre= input("¿Cual es su nombre?")
+lugar= input("¿Donde reside?")
+
+print(f"Hola,soy {nombre} tengo {edad} años y vivo en {lugar}")
+
+#Ejercicio4
+
+radio=int(input(" ingrese el radio"))
+perimetro= int(2* 3.14 *radio)
+area=int(3.14*(radio**2))
+
+print( perimetro)
+print( area)
+
+#Ejercicio5
+
+seg= int(input("ingrese los segundos"))
+hora=int(seg/3600)
+print(f"{hora}hs")
+
+#Ejercicio6
+a= input("ingrese un numero del 1 al 10")
+print(a*1)
+print(a*2)
+print(a*3)
+print(a*4)
+print(a*5)
+print(a*6)
+print(a*7)
+print(a*8)
+print(a*9)
+print(a*10)
